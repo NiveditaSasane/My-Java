3306ba5c2ee6fb1605e94a009f4150b3ee346ed7
NiveditaSasane <113010141+NiveditaSasane@users.noreply.github.com>
2022-10-06 14:56:16 +0530

    Add files via upload

---
+++ lab 06-10-22/inheritance/Parent.java
@@ -0,0 +1,18 @@
+/*
+ create a class parent with default constructor and void sing method.print something insidethe constructor and method
+ @uther : Nivedita Sasane
+ date : 06-10-2022
+ */
+
+package inheritance;
+
+public class Parent {
+	//creating constructor
+	void sing()
+	{
+	System.out.println("I love Singing");
+	
+	}
+	
+
+}
---
+++ lab 06-10-22/inheritance/Parent_main.java
@@ -0,0 +1,21 @@
+/*
+ create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program
+ @uther : Nivedita Sasane
+ date: 06-10-2022
+ */
+
+package inheritance;
+
+public class Parent_main extends Child {
+	public static void main(String[]args)
+	{
+	//creating objects
+		Parent_main P = new Parent_main();
+		//calling the methods
+		P.sing();
+		P.speak();
+		P.dance();
+	}
+	
+
+}
