f7280bfafd84da38a5bfff1f45b4f8c01e558254
NiveditaSasane <113010141+NiveditaSasane@users.noreply.github.com>
2022-10-13 15:28:32 +0530

    Update Natural.java

--- lab 13-10-2022/Natural_number/Natural.java
+++ lab 13-10-2022/Natural_number/Natural.java
@@ -1,13 +1,18 @@
+/*
+ print sum of 10 natural number using while loop.
+   name : nivedita sasane 
+  date : 13/10/2022.
+ */
 package Natural_number;
 import java.util.Scanner;
 public class Natural {
 	public static void main(String[]args) {
-	Scanner sc = new Scanner(System.in);	
+	Scanner sc = new Scanner(System.in);	//user defined
 		
-		int num = 0;
+		int num = 0;//take integer
 		int x;
 		int y=1;
-		while(y<=10)
+		while(y<=10)//using while loop.
 		{
 			System.out.print("enter number:");
 			x = sc.nextInt();
