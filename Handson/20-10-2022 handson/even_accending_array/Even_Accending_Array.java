8d5140214714939772cc603d66d1e5527ee5eac6
NiveditaSasane <113010141+NiveditaSasane@users.noreply.github.com>
2022-10-20 14:31:56 +0530

    Update Even_Accending_Array.java

--- Handson/20-10-2022 handson/even_accending_array/Even_Accending_Array.java
+++ Handson/20-10-2022 handson/even_accending_array/Even_Accending_Array.java
@@ -1,3 +1,8 @@
+/*
+Question : Even Ascending order in array.
+Name : Nivedita sasane.
+date : 20/10/2022.
+
 package even_accending_array;
 //import java.util.Arrays;
 import java.util.*;
@@ -12,14 +17,14 @@ public class Even_Accending_Array {
 		//int array[]=new int[a];
 		Integer Array[]=new Integer [a];
 		System.out.println("enter the elements ");
-		for(int i =0;i<a;i++)
+		for(int i =0;i<a;i++)// for loop
 		{
 			int p=sc.nextInt();
 		    Array[i]=p;
 			
 	}      
 		Arrays.sort(Array);
-		System.out.println("even number in ascending order:");
+		System.out.println("even number in ascending order:");//ascending order
 		
 		for(int j =0;j<a;j++)
 		{
