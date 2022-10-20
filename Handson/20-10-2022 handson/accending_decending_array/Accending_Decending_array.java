10661f4fb8cf7c9b126fc13dbf399093d16979c7
NiveditaSasane <113010141+NiveditaSasane@users.noreply.github.com>
2022-10-20 13:48:51 +0530

    Add files via upload

---
+++ Handson/20-10-2022 handson/accending_decending_array/Accending_Decending_array.java
@@ -0,0 +1,34 @@
+package accending_decending_array;
+//import java.util.Arrays;
+import java.util.*;
+public class Accending_Decending_array {
+public static void main(String[]args) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("enter the no of element in array ");
+			
+			int a = sc.nextInt();
+			//int array[]=new int[a];
+			Integer Array[]=new Integer [a];
+			
+			System.out.println("enter the elements ");
+			
+			for(int i =0;i<a;i++)
+			{
+				Array[i]=sc.nextInt();
+				
+			}
+			System.out.println("Original array"+Arrays.toString(Array));
+			
+			Arrays.sort(Array);
+			System.out.println("Sorted array is"+Arrays.toString(Array));
+			
+			Arrays.sort(Array,Collections.reverseOrder());
+			System.out.println("Sorted array is"+Arrays.toString(Array));
+		}
+			
+
+	}
+
+
+
---
+++ Handson/20-10-2022 handson/even_accending_array/Even_Accending_Array.java
@@ -0,0 +1,35 @@
+package even_accending_array;
+//import java.util.Arrays;
+import java.util.*;
+public class Even_Accending_Array {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter the no of element in array ");
+		
+		int a = sc.nextInt();
+		//int array[]=new int[a];
+		Integer Array[]=new Integer [a];
+		System.out.println("enter the elements ");
+		for(int i =0;i<a;i++)
+		{
+			int p=sc.nextInt();
+		    Array[i]=p;
+			
+	}      
+		Arrays.sort(Array);
+		System.out.println("even number in ascending order:");
+		
+		for(int j =0;j<a;j++)
+		{
+			if (Array[j]%2==0) {
+				System.out.println(Array[j]+" ");
+						
+				
+			}
+			
+		
+		}
+}
+}
