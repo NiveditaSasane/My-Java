357513289bfbf002f6075ab43a4633fec0c910fe
NiveditaSasane <113010141+NiveditaSasane@users.noreply.github.com>
2022-10-21 23:22:18 +0530

    Add files via upload

---
+++ Handson/Array_List.java
@@ -0,0 +1,26 @@
+package Nivedita;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array_List {
+
+	public static void main(String... args)
+	{
+		Scanner sc = new Scanner(System.in);//importing scanner class
+		System.out.println("Enter three number ::");
+		int var1 = sc.nextInt();
+		for(int var=3;var<=3;var++)
+		{
+		System.out.println("Type Only Three Number to Display cars Name ::");
+		ArrayList<String> cars = new ArrayList<String>();// Using List  
+	    int a = sc.nextInt();
+	    System.out.println("[ BMW ]");
+	    int b = sc.nextInt();
+	    System.out.println("[ AUDI ]");
+	    int c = sc.nextInt();
+	    System.out.println("[ FORD ]");
+	}
+	}
+}
+
